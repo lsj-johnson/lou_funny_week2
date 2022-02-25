264894be6505b2d1ef3d7ab74c490d4f70b49589
Lu Johnson <lucasjohnson0613@gmail.com>
2022-02-25 15:46:00 -0700

    1st attempt at welcome? method - failed

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -18,4 +18,25 @@ class OpenMic
   def welcome(performer)
     @performers.append(performer)
   end
+
+  def repeated_jokes?
+    # create array of all jokes
+    joke_list = []
+    @performers.each do |performer|
+      joke_list += performer.jokes
+    end
+
+    # create an array of unique jokes
+    unique_jokes = []
+    @performers.each do |performer|
+      unique_jokes = union(unique_jokes, perfomer.jokes)
+    end
+
+    # compare lengths of two joke lists to see if duplicates
+    if joke_list.length > unique_jokes.length
+      return true
+    else
+      return false
+    end
+  end
 end
--- spec/open_mic_spec.rb
+++ spec/open_mic_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe OpenMic do
     @open_mic = OpenMic.new({location: "Comedy Works", date: '11-20-18'})
     @user_1 = User.new('Sal')
     @user_2 = User.new('Ali')
+    @joke_1 = Joke.new(22, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(13, "How do you keep a lion from charging?", "Take away its credit cards.")
   end
 
   it 'creates an open mic event' do
@@ -21,5 +23,18 @@ RSpec.describe OpenMic do
     @open_mic.welcome(@user_2)
 
     expect(@open_mic.performers).to eq([@user_1, @user_2])
+  end
+
+  it 'can tell if same joke is used multiple times' do
+    @user_2.learn(@joke_1)
+    @user_2.learn(@joke_2)
+
+    expect(@open_mic.repeated_jokes?).to eq(false)
+  end
+
+  it 'can tell if same joke is used multiple times 2' do
+    @user_1.learn(@joke_1)
+
+    expect(@open_mic.repeated_jokes?).to eq(true)
   end
 end
